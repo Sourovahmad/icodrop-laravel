5a72c97b9579e65511643d860bb230b54c22898d
sourovahmad <mdsourovahmad19@gmail.com>
2021-12-24 19:02:25 +0600

    init

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,51 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="logo">
+                    <img src="LogoDrops.png" alt="">
+                </div>
+                <div class="descs">
+                    <p>Made with  for the Decentralized World.</p>
+                </div>
+                <div class="descs">
+                    <p>ICO Drops is an independent ICO (Token Sale) database and is not affiliated with any ICO project or company. Our Interest Level does not constitute financial or investment advice.</p>
+                </div>
+                <div class="descs">
+                    <p>ICO Drops receives a fee for advertising certain token sales, in which case such listing will be designated accordingly.</p>
+                </div>
+                <div class="descs">
+                    <p>© 2021 ICO Drops.© 2021 ICO Drops.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <ul class="page-links">
+                    <li><a href="">ACTIVE ICO</a></li>
+                    <li><a href="">UPCOMING ICO</a></li>
+                    <li><a href="">ENDED ICO</a></li>
+                    <li><a href="">BOUNTY LIST (NEW)</a></li>
+                    <li><a href="">SENDBOX (BETA)</a></li>
+                    <li><a href="">WHITE LIST (BETA)</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <ul class="page-links">
+                    <li><a href="pages/about.html">ABOUT</a></li>
+                    <li><a href="">ADVERTISING</a></li>
+                    <li><a href="">ENDED ICO</a></li>
+                    <li><a href="">SUBMIT ICO</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <ul class="page-links">
+                    <li><a href="">EMAIL</a></li>
+                    <li><a href="">TWITTER</a></li>
+                    <li><a href="">TELEGRAM NEWS</a></li>
+                    <li><a href="">TELEGRAM CHAT</a></li>
+                    <li><a href="">TELEGRAM CHAT(RU)</a></li>
+                    <li><a href="">DROPSERN CHAT(RU)</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <div class="logo">
+                <img src="LogoDrops.png" alt="">
+            </div>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <img src="menu.svg" alt="">
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto">
+                <a class="nav-link" href="">HOME</a>
+                <a class="nav-link" href="pages/about.html">ABOUT</a>
+                <a class="nav-link" aria-current="page" href="pages/active.html">ACTIVE ICO</a>
+                <a class="nav-link" href="pages/upcoming.html">UPCOMING ICO</a>
+                <a class="nav-link" href="pages/ended.html">ENDED ICO</a>
+                <a class="nav-link" href="pages/whitelist.html">WHITELIST</a>
+            </div>
+        </div>
+    </div>
+</nav>
+<header>
+    <div class="container">
+        <div class="situation d-flex">
+            <span class="">BTC $49143 <span class="greenNumber">+07%</span></span>
+            <span>ETH $4002  <span class="redNumber">-0.6%</span></span>
+        </div>
+        <div class="smallDesc">
+            <p><span class="redNumber">*</span> DISCLAIMER: All information including our "Interest Level" rating, is provided merely for informational purposes. ICO Drops does not provide investment advice </p> 
+        </div>
+    </div>
+</header>
